ffcf37ed17e1b69562aa66d13f7d3b8a14558c62
Bambina1000 <98654566+Bambina1000@users.noreply.github.com>
2026-07-25 00:52:47 +0000

    Switch to Turso cloud database

--- database.py
+++ database.py
@@ -1,14 +1,21 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-# SQLite database file stored right in your project folder
-SQLALCHEMY_DATABASE_URL = "sqlite:///./journal.db"  # <-- FIXED: removed the ".."
+# Turso connection details
+TURSO_DATABASE_URL = os.environ.get("TURSO_DB_URL", "libsql://nek-journal-bambina1000.aws-us-west-2.turso.io")
+TURSO_AUTH_TOKEN = os.environ.get("TURSO_AUTH_TOKEN", "eyJhbGciOiJFZERTQSIsInR5cCI6IkpXVCJ9.eyJhIjoicm8iLCJpYXQiOjE3ODQ5NDA1MjEsImlkIjoiMDE5Zjk2YmQtZTAwMS03NzY5LWIzNjUtODhmNDcyN2M4OTc0Iiwia2lkIjoiMVJnTnY0aEYycXFlbko4TU1Zd1FfT1RVM0NoZkVDdzNLdFRiOXg4YVIzYyIsInJpZCI6IjJmZjY0ZWQ3LWMzNzAtNGVlZi1hOTliLTFiN2FhNGFkODdlOSJ9.xAdDoKKJ7VIlYwf1n8UMsVvRCoEKBZ0GyJ0lw4jrfiAeIMax-ljnsSGunbgrc4yFgQ-Ng6jnxmYFjN3XezxaDA")
 
-# connect_args is needed only for SQLite to allow multi-threaded access
+# Create the engine using the libsql dialect
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+    TURSO_DATABASE_URL,
+    connect_args={
+        "auth_token": TURSO_AUTH_TOKEN,
+    },
+    echo=False,
 )
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
